53114cad7dde3d15414f119c1d174c7953bd0cce
Abdoulaye <njigouhrazak@iut-dhaka.edu>
2021-05-07 06:02:41 +0100

    added more styling and content

--- js/front.js
+++ js/front.js
@@ -67,3 +67,16 @@ $(function () {
      }
 
  }
+
+window.onscroll = function() {myFunction()};
+
+let header = document.getElementById("mynav");
+let sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
